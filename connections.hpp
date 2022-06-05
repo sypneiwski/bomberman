c7e71dbdada14cad3b889e1fac8f26acaa012e91
sypniewski <m.odrowaz.sypniewski@gmail.com>
2022-06-05 15:53:56 +0200

    changed tabs to spaces

--- connections.hpp
+++ connections.hpp
@@ -10,38 +10,38 @@
 
 class BufferError : public std::exception {
 public:
-	const char * what () const throw () {
-		return "Buffer error.";
-	}
+  const char * what () const throw () {
+    return "Buffer error.";
+  }
 };
 
 // Helper class for serializing outgoing messages.
 // The writeX functions convert binary numbers to network order.
 class Buffer {
 public:
-	static constexpr size_t BUFFER_SIZE = 65535;
-	Buffer();
+  static constexpr size_t BUFFER_SIZE = 65535;
+  Buffer();
 
-	Buffer &write8(uint8_t);
+  Buffer &write8(uint8_t);
 
-	Buffer &write16(uint16_t);
+  Buffer &write16(uint16_t);
 
-	Buffer &write32(uint32_t);
+  Buffer &write32(uint32_t);
 
-	Buffer &write_string(std::string);
+  Buffer &write_string(std::string);
 
-	size_t size() const;
+  size_t size() const;
 
-	char* get_data();
+  char* get_data();
 
-	void clear();
+  void clear();
 
 private:
-	char data[BUFFER_SIZE];
-	char *ptr, *end;
+  char data[BUFFER_SIZE];
+  char *ptr, *end;
 
-	template<typename T>
-	void write(T);
+  template<typename T>
+  void write(T);
 };
 
 // Abstract base class for connections.
@@ -49,59 +49,59 @@ private:
 // and write to sockets.
 class Connection {
 public:
-	virtual void write(Buffer &buffer) = 0;
+  virtual void write(Buffer &buffer) = 0;
 
-	Connection& read8(uint8_t&);
+  Connection& read8(uint8_t&);
 
-	Connection& read16(uint16_t&);
+  Connection& read16(uint16_t&);
 
-	Connection& read32(uint32_t&);
+  Connection& read32(uint32_t&);
 
-	Connection& read_string(std::string&);
+  Connection& read_string(std::string&);
 
-	virtual void close() = 0;
+  virtual void close() = 0;
 
 protected:
-	virtual void read(void*, size_t) = 0;	
+  virtual void read(void*, size_t) = 0;	
 };
 
 // Class wrapping the boost UDP socket.
 class UDPConnection : public Connection {
 public:
-	UDPConnection(boost::asio::io_context&, Options&);
+  UDPConnection(boost::asio::io_context&, Options&);
 
-	void write(Buffer&) override;
+  void write(Buffer&) override;
 
-	bool has_more() const;
+  bool has_more() const;
 
-	void close() override;
+  void close() override;
 
 private:
-	using udp = boost::asio::ip::udp;
-	udp::socket socket;
-	udp::endpoint endpoint;
+  using udp = boost::asio::ip::udp;
+  udp::socket socket;
+  udp::endpoint endpoint;
 
-	static constexpr size_t MAX_UDP = 65535;
-	char data[MAX_UDP];
-	char *ptr, *end;
+  static constexpr size_t MAX_UDP = 65535;
+  char data[MAX_UDP];
+  char *ptr, *end;
 
-	void read(void*, size_t) override;
+  void read(void*, size_t) override;
 };
 
 // Class wrapping the boost TCP socket
 class TCPConnection : public Connection {
 public:
-	TCPConnection(boost::asio::io_context&, Options&);
+  TCPConnection(boost::asio::io_context&, Options&);
 
-	void write(Buffer&) override;
+  void write(Buffer&) override;
 
-	void close() override;
+  void close() override;
 
 private:
-	using tcp = boost::asio::ip::tcp;
-	tcp::socket socket;
+  using tcp = boost::asio::ip::tcp;
+  tcp::socket socket;
 
-	void read(void*, size_t) override;
+  void read(void*, size_t) override;
 };
 
 #endif // CONNECTIONS_HPP
--- program_options.hpp
+++ program_options.hpp
@@ -5,18 +5,18 @@
 
 class OptionsError : public std::invalid_argument {
 public:
-	OptionsError(std::string const& e) : std::invalid_argument(e) {}
+  OptionsError(std::string const& e) : std::invalid_argument(e) {}
 };
 
 // Struct for parsing and storing all options from the commandline.
 struct Options {
-	std::string gui_address,
-	            gui_port,
-				      player_name,
-				      server_address,
-				      server_port;
-	uint16_t port;
-	Options(int, char*[]);
+  std::string gui_address,
+              gui_port,
+              player_name,
+              server_address,
+              server_port;
+  uint16_t port;
+  Options(int, char*[]);
 };
 
 #endif // PROGRAM_OPTIONS_HPP
